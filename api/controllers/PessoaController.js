54358087c613019eb5093a50ae1c53088f15a64f
Jyeverson Madalozo <jyeversonsantos@gmail.com>
2022-01-02 15:21:09 -0300

    put /pessoas/id (Updating data)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -32,6 +32,18 @@ class PessoaController {
         }
     }
 
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try{
+            await database.Pessoas.update(novasInfos, { where: {id: Number(id)}} )
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: {id: Number(id)} })
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
