f317a5075f3c7429b425d084e2e7419068df5c7f
Jyeverson Madalozo <jyeversonsantos@gmail.com>
2022-01-02 15:33:30 -0300

    delete /pessoas/id (delete pessoas)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -44,6 +44,16 @@ class PessoaController {
         }
     }
 
+    static async apagaPessoa(req,res){
+        const { id } = req.params
+        try{
+            await database.Pessoas.destroy({where: {id: Number(id)}})
+            return res.status(200).json({mensagem: `id ${id} Destroyed`})
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
